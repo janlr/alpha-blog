ac07c37334ff7509f6b3e9c503c7bd7f53b5c504
Jan <jan.leroux0@gmail.com>
2018-05-08 12:01:10 +0000

    Add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,9 +32,39 @@ class CategoriesController < ApplicationController
   end
   
   
+  def edit
+  
+  @category = Category.find(params[:id])
+  
+  
+  end
+  
+  
+  
+  def update
+    
+   @category = Category.find(params[:id])
+   
+   if category.update(category_params)
+     flush[:success] = "Category name was successfully updated."
+     redirect_to category_path(@gategory)
+     
+   else
+     
+     render 'edit'
+     
+     
+   end
+  
+  end
+  
+  
+  
   
   def show
     
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
    
   end
 
